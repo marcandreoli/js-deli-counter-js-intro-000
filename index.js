7595e1d05ba47e51ca610b63034f9359f61dced6
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-29 23:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLine = katzDeliLine +1;
-    katzDeli.push(name);
-  }
-  return katzDeliLine.length;
+  katzDeli.push(name);
+  console.log("you are the ${katzDeli.length}th person")
+  //for (var i = 0; i < katzDeliLine.length; i++) {
+    //katzDeliLine = katzDeliLine +1;
+    //katzDeli.push(name);
+  //}
+  //return katzDeliLine.length;
 }
