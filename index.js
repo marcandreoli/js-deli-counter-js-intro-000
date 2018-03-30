fd47ac262a22abb2cb8513103e4579c2e3a6ba01
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-30 02:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length==0) {
     return "The line is currently empty.";
   } else {
-    return `The line is currently: ${katzDeliLine}.`;
+    for (i = 0; i < katzDeliLine.length; i++) {
+      return `The line is currently: ${i}. ${katzDeliLine}.`
+    }
   }
 }
