5cb3bd6d0ef0aae6f6f22d2de5fbb27e29106a84
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-30 00:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return katzDeliLine;
+  return katzDeli;
   console.log(`Welcome, ${name}. you are number ${katzDeliLine.length} in line`)
 };
 
