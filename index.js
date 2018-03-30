161d5b6e41a85cfe726c979e396ea3bf639aa9b4
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-30 01:48:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function takeANumber(katzDeliLine, name) {
   };
 
 function nowServing(katzDeliLine) {
-  if(katzDeli.length == 0) {
+  if(katzDeliLine.length == 0) {
     return `There is nobody waiting to be served!`;
   } else {
     return `Currently serving ${katzDeli}.`;
-    katzDeli.shift();
+    katzDeliLine.shift();
     return katzDeli;
   }
 };
